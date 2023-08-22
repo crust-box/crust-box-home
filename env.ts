d385e23c4b266761f3abf12203775e7220367d8a
eericxu <eericxu@MacM1.local>
2023-08-22 13:15:56 +0800

    add workflow

--- env.ts
+++ env.ts
@@ -3,12 +3,7 @@ const isBeta = true;
 
 const baseUrl = isBeta ? "https://beta-api.baitech-ipfs.net" : "https://api.baitech-ipfs.net";
 
-const auth = isBeta
-  ? "Bearer c3Vic3RyYXRlLWNUTEJlSGlvd2JDZE1rdjNLaENSQkxzbXNmRDNicVlnVlZURU5DQlp1ZjIxRW5OOEc6MHgwMjFiNTU1OTg3ZGU4OTJlY2JlMmE5MWIzMTI3Mzg4OGIwYTUwYzZmN2ExNzAwNTFhNzVkNjAwMDc2NzhiYjA1YTU0NWIwYjJkNjVkYmRlNTJmNWQyNDU0NzljODRiMzExZDQxMjM5MjU3MzM5MTlhMGFkMzhiZWE0YjRlZGM4OQ"
-  : "Bearer c3Vic3RyYXRlLWNUSjJ3bUg5WmZuOHpLd0M2NnFQanZpRHU4M05yVmdjM0JHTG56emJNOFRSWkxhWmk6MHhiYWNlNzRhMDAwMjc5MDY3MzBlMTc5NmEyNWU2OWU2ZWRjYmIyNzg1MDEwZjVjYzYxOTg0YmQwM2I2NDk5YTE5MTY5MTEyMDdhNDNkNGFmM2MwMWEyNjRhMTRiN2UyOGQ2ZmRhYzExOTJhMmU5YmZiMTc5NjMzNGYxZTE5MGU4Mg==";
-
 export default {
   isBeta,
   baseUrl,
-  auth,
 };
